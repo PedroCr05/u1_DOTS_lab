a22994d643a8ada81548d6a4d0ab8bb0d60a18ed
pedrocruz <JayJMarquez05@gmail.com>
2024-09-23 18:42:56 -0500

    Dots' game finally finished and it works! :D

--- starter/level-one.js
+++ starter/level-one.js
@@ -10,13 +10,12 @@ const levelWinner = document.querySelector(`.level-winner`);
 ball.addEventListener(`click`, ()=>{
     // This is how we are incrementing our score value
     score += 10;
-    // Chaning the text of the score which is set at 0000 by scoreDisplay
-        // Using the score value we change it's origion from another variable
+    // Changing the text of the score which is set at 0000 by scoreDisplay as defaulted
+        // Using the score value, we change it's origin from another variable which is from `score`
     scoreDisplay.innerText = score;
     // Our conditional statement for when our player reaches 100 points!
     if (score >= 100){
-        // Simple DOM here.
-            // Changing the opacity of the text from 0 to 1.
+        // Changing the opacity of the text from 0 to 1.
         levelWinner.style.opacity = 1;
     }
 });
@@ -24,4 +23,4 @@ ball.addEventListener(`click`, ()=>{
 // Notes:
     // This is what a conditional means
         // We aren't trying to make a for statement
-            // Just simple if it's this or not. Nothing more.
+            // It's just a simple `if();` statement. 
--- starter/level-three.js
+++ starter/level-three.js
@@ -7,32 +7,32 @@ const balls = document.querySelectorAll(`.js-ball`);
 const scoreDisplay = document.querySelector(`.js-score`);
 const levelWinner = document.querySelector(`.level-winner`);
 
-// Our for loop to select within our `Array[];`
+// Our `for(loop)` is to select within our `Array[];`
 for (i = 0; i < balls.length; i++){
-    // Calling the object.. `{example.object}` | `{example[ourArrayVariable].object}`
-    // console.log(balls[i].dataset.increment);
+    // Calling the object.. `{example.object}` || `{example[ourArrayVariable].object}`
+    // `console.log(balls[i].dataset.increment)`;
     // The second way of the day same thing above.
 
     // So here is a break down of understanding this process:
         // Below here is our `dataset but in another way`
             // console.log(balls[i].getAttribute('data-increment'));
-        // Since we get our number values from running it through the console.log, we can use this basically reuse it within our code
+        // Since we get our number values from running it through the `console.log();`, and since it does output our values. We can reuse it within our code
             // const ballVal = parseInt(balls[i].getAttribute('data-increment'));
-        // We remove the `console.log` & define this line of code into a variable. Hence the `let ballVal`
-        // With `ballVal` it holds the value of whatever the console.log spat out to us.
+        // We now can remove the `console.log();` && define this line of code into a variable. Hence the `const ballVal`
+        // With `ballVal` it holds the value of whatever the `console.log()` spat out to us.
         // In this case, it was the values of `20, 5, 10;`
-        // Now since we did that. We aren't actually finished. I have to turn my number values into actual number values
-        // Without `parseInt()` | It'll change the number values as a `string` value which isn't what we need.
-            // Our parseInt() has to be infront, so it works properly
+        // Now since we did that. We aren't actually finished. We have to turn the output value into actual number values
+        // Without `parseInt()`, It'll change the number values as a `string` value which isn't what we need.
+            // Our `parseInt()` has to be infront, so it works properly
     const ballVal = parseInt(balls[i].getAttribute('data-increment'));
-    // Our `.addEventListener li` that is checking for our values
+    // Our `.addEventListener() li` is checking for our actual number values
     balls[i].addEventListener(`click`, ()=>{
-        // Now here is where we use our BallVall instead of doing the hard code of `10;`
+        // Now here is where we use our function called `ballVal` instead of doing the usual hard code of `10;`
             // Just a small reminder. Whenever an input is given. It'll spit out as an `Array[];` order, so order placement does matter!
         score += ballVal;
         // Our display for the player score
         scoreDisplay.innerText = score;
-        // The conditional statement for when the player passes the level
+        // The conditional statement for when the player wins during the level
         if(score >= 100){
             // Changing the value of our opacity from `levelWinner` so it can appear on screen again
             levelWinner.style.opacity = 1;
@@ -40,7 +40,7 @@ for (i = 0; i < balls.length; i++){
     })
 }
 
-Full code without the comment in between
+// Full code without the comment in between
 let score = 0;
 
 const balls = document.querySelectorAll(`.js-ball`);
@@ -58,18 +58,18 @@ for (i = 0; i < balls.length; i++){
     })
 }
 
-// Now this is by using the `forEach.();` instead of a for loop
+// Now this is by using the `forEach.(); function();` instead of a `for(loop);`
 let score = 0;
 
 const balls = document.querySelectorAll(`.js-ball`);
 const scoreDisplay = document.querySelector(`.js-score`);
 const levelWinner = document.querySelector(`.level-winner`);
 
-// Here we are declaring the `forEach();` statement which acts like an `Array[] li`
+// Here we are declaring the `forEach();` statement which acts like an `Array[];`
     // We decalre jsBall since it's a placeholder for every value that is inputed
 balls.forEach(jsBall => {
-    // Instead of using `balls` as our `functionName();` it preset's the `Array[];`
-        // Though when running this code. Our `Array[];` it would be considered a string we have to run it with `parseInt();`
+    // Instead of using `balls` as our `functionName();` it preset as the `Array[];`
+        // Though when running this code. Our `Array[];` would be considered a string. Which is why we have to run it with `parseInt();` once again.
     const ballVal = parseInt(jsBall.getAttribute('data-increment'));
     
     // Same process with `ballVal`
@@ -83,6 +83,5 @@ balls.forEach(jsBall => {
 });
 
 // Additional Notes:
-    // So here's the thing.
-    // When calling an object | {example.object} <- | It's basically searching within folders.
-        // When you run `console.log(function.dataset);` | It'll give you the name of the object to that folder and from there is rinse and repeat.
+    // When calling an object | {example.object} <- It's basically searching within folders in a way.
+        // When you run `console.log(function.dataset);` <- It'll give you the name of the object to that folder and from there is rinse and repeat.
